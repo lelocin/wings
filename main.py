a07b2ead18a889e1c54f9cfa6b44ad78a1aed16e
Nicole Lai <nl2321@nyu.edu>
2024-10-07 20:52:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ import mysql.connector
 from mysql.connector import Error
 
 load_dotenv()
+
+def createConnection():
+  connection = None
+   try:
+        print(f"Attempting to connect to:")
+        print(f"Host: {os.getenv('DB_HOST')}")
+        print(f"User: {os.getenv('DB_USER')}")
+        print(f"Database: {os.getenv('DB_NAME')}")
+        
+        connection = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+            database=os.getenv('DB_NAME')
+        )
+        print("Successfully connected to the database")
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        print(f"Error Code: {e.errno}")
+        print(f"SQL State: {e.sqlstate}")
+        print(f"Error Message: {e.msg}")
+    return connection
